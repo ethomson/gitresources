349b51a7f61fea517976934370c6937dfc7a0258
Carol <carol@example.com>
2015-08-03 15:56:55 -0400

    Further remove user management

--- GitResources/Scripts/app/home.viewmodel.js
+++ GitResources/Scripts/app/home.viewmodel.js
@@ -1,23 +1,7 @@
 ﻿function HomeViewModel(app, dataModel) {
     var self = this;
 
-    self.myHometown = ko.observable("");
-
     Sammy(function () {
-        this.get('#home', function () {
-            // Make a call to the protected Web API by passing in a Bearer Authorization Header
-            $.ajax({
-                method: 'get',
-                url: app.dataModel.userInfoUrl,
-                contentType: "application/json; charset=utf-8",
-                headers: {
-                    'Authorization': 'Bearer ' + app.dataModel.getAccessToken()
-                },
-                success: function (data) {
-                    self.myHometown('Your Hometown is : ' + data.hometown);
-                }
-            });
-        });
         this.get('/', function () { this.app.runRoute('get', '#home') });
     });
 
