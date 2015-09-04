6dea8c154f371bd2483a72a08a7f842a3527a447
Edward <ethomson@edwardthomson.com>
2015-09-04 09:53:01 -0700

    *Really* remove authentication

--- GitResources/Startup.cs
+++ GitResources/Startup.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Owin;
-using Owin;
 
 namespace GitResources
 {
     public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
-        {
-        }
     }
 }
